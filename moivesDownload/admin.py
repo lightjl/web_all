2b733a247defaf700402d77abf7d187d32520384
jlgs-jz <jlgs-jz@jlgs-jz-PC>
2018-01-07 22:07:33 +0800

    add *admin to look better

--- moivesDownload/admin.py
+++ moivesDownload/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from moivesDownload.models import Moive, People, Watch, Statue_dm
 # Register your models here.
 
-admin.site.register(Moive)
+class WatchAdmin(admin.ModelAdmin):
+    list_display = ('moive', 'statue')
+    list_filter = ('statue',)
+    fields = ('statue',)
+
+class MoiveAdmin(admin.ModelAdmin):
+    list_display = ('name_En', 'downloadLink')
+    fields = ('name_Zh',)
+    
+admin.site.register(Moive, MoiveAdmin)
 admin.site.register(People)
-admin.site.register(Watch)
+admin.site.register(Watch, WatchAdmin)
 admin.site.register(Statue_dm)
