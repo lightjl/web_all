8f6a3c09c6943af15ae0e225e983a66622eaa97e
jlgs-jz <jlgs-jz@192.168.16.149>
2018-01-10 22:37:50 +0800

    add dataTables to show

--- moivesDownload/urls.py
+++ moivesDownload/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-import moivesDownload.urls
-from django.conf.urls import url, include
-from django.conf import settings
-from django.conf.urls.static import static
+from . import fixDatebaseFromMail
+from . import mj
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('mv/', include('moivesDownload.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('show/', views.show),
+    path('download/', mj.loginAndDownload),
+]
--- moivesDownload/views.py
+++ moivesDownload/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+
+from django.http import HttpResponse
+from moivesDownload.models import Moive, People, Watch, Statue_dm
 
 # Create your views here.
+
+def show(request):  # show
+    
+    return render_to_response('moivesDownload/list.html', {'moives':Moive.objects.all()})
